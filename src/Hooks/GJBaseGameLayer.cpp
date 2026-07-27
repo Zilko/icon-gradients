6f56399009343aa92d92557c7efaa000d9a22a3a
Zilko <69888540+Zilko@users.noreply.github.com>
2026-07-27 14:12:13 -0600

    aaaaaaaaaaaa

--- src/Hooks/GJBaseGameLayer.cpp
+++ src/Hooks/GJBaseGameLayer.cpp
@@ -18,7 +18,7 @@ void ProGJBaseGameLayer::playExitDualEffect(PlayerObject* p0) {
     if (auto icon = static_cast<ProSimplePlayer*>(f->dualSimplePlayer)) {
         Utils::applyGradient(
             icon,
-            Utils::getGradient(icon->m_fields->m_type, p0 == m_player2);,
+            Utils::getGradient(icon->m_fields->m_type, p0 == m_player2),
             true,
             p0 == m_player2,
             1000
